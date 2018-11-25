caa83a4cb4a02688ba694ff3fbb51a071545a924
Leonardo Schimpf Machado <leonardo.schimpf@gmail.com>
2018-11-25 16:12:28 -0200

    k-Fold cross validation implementado

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 import data_utils
+import metrics_utils
 import pipeline as pip
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
 
 data, labels = data_utils.load_data_set_from_arff('./database/OffComBR2.arff')
 yes_ratio = data_utils.get_yes_ratio(labels)
 print("Distribuição de classes:\n\t'sim': {:.3f}\n\t'não': {:.3f}\n".format(yes_ratio, 1 - yes_ratio))
-
-# Separate into train and test datasets maintaining proportion
-X_train, X_test, y_train, y_test = data_utils.split_test_train(data, labels, test_prop=0.2)
 
 # [TODO] Declarar outros classificadores
 classifiers = {"Naive-Bayes" : MultinomialNB(),
@@ -23,12 +22,20 @@ for classifier_name, classifier in classifiers.items():
     # Pipeline convert to a frequency-based bag of words and append classifier
     pipeline = pip.get_pipeline(classifier)
 
-
     # [TODO] Tunar parâmetros utilizando dataset de test e validação - ex: crossfold
-    # Importante: Não utilizar conjunto de teste para tunar parâmetros - somente utilizar teste para comparar modelos
-    pipeline.fit(X_train, y_train)
-    predicted = pipeline.predict(X_test)
+    qty_cross_validations = 10
+    kf = KFold(n_splits = qty_cross_validations, shuffle=True)
+    accuracy_collection = []
 
-    # [TODO] Colocar mais medidas de qualidade, em especial as que lidam com datasets desbalanceados
-    test_accuracy = accuracy_score(y_test, predicted)
-    print("Acurácia: {:.3f}".format(test_accuracy), end="\n\n")
+    print("Usando {:d}-fold-cross-validation...".format(qty_cross_validations))
+    for train_index, test_index in kf.split(data):
+        X_train, X_test = data[train_index], data[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+        pipeline.fit(X_train, y_train)
+        predicted = pipeline.predict(X_test)
+
+        # [TODO] Colocar mais medidas de qualidade, em especial as que lidam com datasets desbalanceados
+        accuracy_collection.append(accuracy_score(y_test, predicted))
+
+    accuracy_mean = metrics_utils.calculate_accuracy_mean(accuracy_collection)
+    print("Acurácia: {:.3f}".format(accuracy_mean), end="\n\n")
---
+++ metrics_utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def calculate_accuracy_mean(accuracy_collection):
+    return np.mean(np.array(accuracy_collection))
